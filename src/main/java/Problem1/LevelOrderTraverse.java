d82fe9046a57c1ec11d39600f15b39d0101db752
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-11 20:17:20 -0800

    LevelOrderTraverse Progress

--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -6,12 +6,38 @@ package Problem1;
     Homework 5
  */
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        return result;  // place holder
+        Queue<TreeNode> discoveredNodes = new LinkedList<TreeNode>();
+
+        if(root == null){
+            return result;
+        } else {
+           discoveredNodes.add(root);
+        }
+
+        while(!discoveredNodes.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            temp.add((int) discoveredNodes.poll().val);
+            result.add(temp);
+
+            if(root.left != null){
+                discoveredNodes.add(root.left);
+            }
+
+            if(root.right != null){
+                discoveredNodes.add(root.right);
+            }
+
+        }
+
+
+        return result;
     }
 }
