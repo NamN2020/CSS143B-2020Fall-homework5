13a6107c9acbde64d712e71c5e372d68a5f24085
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-14 14:08:04 -0800

    LCS Complete

--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -4,6 +4,8 @@ package Problem2;
     November 15, 2020
     CSS 143B
     Homework 5
+
+    Sources: https://youtu.be/py3R23aAPCA
  */
 import Problem1.TreeNode;
 
@@ -11,6 +13,22 @@ public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
         // hint: pay attention to the assumptions of this problem.
-        return null;    // place holder
+        if(root == null){
+            return null;
+        } else if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode leftSearch = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightSearch = lowestCommonAncestor(root.right, p, q);
+
+
+        if(leftSearch != null && rightSearch != null){
+            return root;
+        } else if(rightSearch == null){
+            return leftSearch;
+        } else {
+            return rightSearch;
+        }
     }
 }
