c041d59d65363bd1d556a599f2bde8a1c740f399
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-15 11:05:38 -0800

    InsertInBST Complete

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -5,10 +5,37 @@ package Problem3;
     CSS 143B
     Homework 5
  */
+
 import Problem1.TreeNode;
 
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
         // homework
+        TreeNode val = new TreeNode(valToInsert);
+        TreeNode<Integer> prevNode = new TreeNode(root);
+
+        if(root == null){
+            root = val;
+        }
+
+        while (root != null) {
+            prevNode = root;        // hold prevNode since loop goes until root == null
+
+            if (valToInsert > root.val) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
+        // insert node in correct position
+        if (valToInsert > prevNode.val) {
+            prevNode.right = val;
+        } else if (valToInsert < prevNode.val) {
+            prevNode.left = val;
+        } else {    // if key == root, insert key either direction
+            prevNode.left = val;
+        }
+
     }
 }
